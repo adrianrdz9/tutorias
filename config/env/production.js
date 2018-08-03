f3c1170db81232d7d51659a172ea0ff3be178f19
Adrian Rodriguez <adrianrodriguez@Adrians-MacBook-Air.local>
2018-08-03 13:56:03 -0500

    Locals faltantes

--- config/env/production.js
+++ config/env/production.js
@@ -149,7 +149,7 @@ module.exports = {
     ***************************************************************************/
     cors: {
       allowOrigins: [
-         'https://tutoress.herokuapp.com/',
+         'https://tutoress.herokuapp.com',
       ]
     },
 
